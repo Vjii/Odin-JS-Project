9ea8bfdd20e0336cff3c35be24eb8be568e24481
Jan Pilik <snyshuem@gmail.com>
2016-12-18 13:57:31 +0100

    make each repeated pass over same square add 10% black to it

--- js/main.js
+++ js/main.js
@@ -1,7 +1,4 @@
-var i = 1; 
-var squareCount = 16;
-
-function rgbColor() {
+function rngColor() {
 	var letters = "ABCDF0123456789".split("");
 	
 	rng = []
@@ -13,24 +10,36 @@ function rgbColor() {
 	return rng;
 }
 
-rgbColor();
-
+var i = 1; 
+var squareCount = 16;
 
 function newGrid(squareCount) {
 	
-	var container = document.createElement('div');
+	var container = document.createElement("div");
 	squareCount = squareCount * squareCount
 
 	container.setAttribute("class", "square-container");
-
+	var y = 100;
+	var track = [];
+	squareIndex = 0;
 	for (var i=1; i <= squareCount; i++) { 
-		var square = document.createElement('div');
-		square.setAttribute("class", "square");
+		var square = document.createElement("div");
+		squareIndex++;
+		square.setAttribute("class", "square")
+		square.setAttribute("id", "'" + squareIndex + "'");
 		size = (400 / Math.sqrt(squareCount)) - 2;
 		square.style.width = size + "px";
 		square.style.height = size + "px";
-		square.addEventListener("mouseleave", function() {
-			this.style.backgroundColor = rgbColor();
+		square.addEventListener("mouseover", function() {
+			this.style.backgroundColor = rngColor();			
+			
+			if (track.includes(this) === true) {
+				y -= 10;
+				this.style.filter = "brightness(" + y + "%)";
+				alert(y);
+			}
+
+			track.push(this);
 		});
 
 		container.appendChild(square);
@@ -40,9 +49,10 @@ function newGrid(squareCount) {
 
 newGrid(squareCount);
 
+
 function newUserGrid() {
 	document.getElementById("wrapper").innerHTML= " ";
-	 squareCount = prompt("How many squares per side would you like for your shiny new grid? Provide a number!")
+	var squareCount = prompt("How many squares per side would you like for your shiny new grid? Provide a number!")
 	newGrid(squareCount)
 
 };
